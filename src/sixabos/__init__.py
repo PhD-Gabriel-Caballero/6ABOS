02a8e59f1d31117673245f9eefc86a596da783a9
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-15 10:50:29 +0100

    Update __init__.py

--- src/sixabos/__init__.py
+++ src/sixabos/__init__.py
@@ -6,3 +6,23 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""6ABOS Atmospheric processing framework. Software package developed by UV."""
+
+__version__ = "0.1.0"
+__author__ = "Gabriel Caballero"
+__email__ = 'gabriel.caballero@uv.es'
+
+# Main classes for easy access
+from .models import AtmosphericCorrection
+
+
+
