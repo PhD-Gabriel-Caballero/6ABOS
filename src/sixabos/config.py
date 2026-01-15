b04e6d90eab830a23dc62380dc7e4cd73187d3d1
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-15 11:08:22 +0100

    Create config.py

---
+++ src/sixabos/config.py
@@ -0,0 +1,14 @@
+
+DEFAULT_CONF = {
+    "verbose": True,
+    "data_plotting": False,
+    "data_storing": True,
+    "GEE": False,
+    "max_wavelength": 2480,
+    "min_wavelength": 379,
+    "wavelength_step": 2.5,
+    "tgas_threshold": 0.75,
+    "input_dir": None,
+    "output_dir": None,
+    "output_rrs": False
+}
