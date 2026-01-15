1bcb9a585994df4fab4207a58596bc50b3650f34
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-15 10:33:49 +0100

    Update __init__.py

---
+++ src/sixabos/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# 6ABOS: 6S-based Atmospheric Background Offset Subtraction for Atmospheric Correction
+# Copyright (C) 2026 Gabriel Caballero (University of Valencia)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
