fef520f17ffae53c485aec5d11bc8a093dbe0783
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-15 11:28:54 +0100

    Update __init__.py

--- src/sixabos/__init__.py
+++ src/sixabos/__init__.py
@@ -9,7 +9,7 @@
 
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 # details.
 #
 # You should have received a copy of the GNU General Public License along
@@ -23,3 +23,4 @@ from .config import DEFAULT_CONF
 
 __version__ = "1.0.0"
 __all__ = ["run_6abos", "SixABOSEngine", "DEFAULT_CONF"]
+
