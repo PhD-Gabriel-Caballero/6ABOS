f970d80970b32b71ef391e235a83cdcf9d6554d0
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-26 15:13:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-# Get the long description from the README file
+# Leer la descripción larga desde el README
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -9,14 +9,12 @@ setup(
     name="sixabos",
     version="1.1.0",
     author="Gabriel Caballero",
-    author_email="gabriel.caballero@uv.es",  
-    description="6S-based Atmospheric Background Offset Subtraction for EnMAP hyperspectral imagery atmospheric correction",
+    author_email="gabriel.caballero@uv.es",
+    description="6S-based Atmospheric Background Offset Subtraction",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PhD-Gabriel-Caballero/6ABOS",
-    project_urls={
-        "Bug Tracker": "https://github.com/PhD-Gabriel-Caballero/6ABOS/issues",
-    },
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
@@ -29,27 +27,15 @@ setup(
     ],
     python_requires=">=3.10",
     
-    # Core dependencies
     install_requires=[
         "numpy",
-        "scipy",
         "pandas",
+        "scipy",
         "matplotlib",
         "xmltodict",
-        "tqdm",
+        "py6s",
+        "earthengine-api",
         "gdal", 
     ],
-    
-    # Automatically find the S6ABOS_package
-    packages=find_packages(),
-    
-    # If you have data files (like 6S lookup tables or XML templates)
     include_package_data=True,
-    
-    # This allows users to run your code from the terminal
-    entry_points={
-        "console_scripts": [
-            "sixabos-run=S6ABOS_package.main:main", 
-        ],
-    },
 )
