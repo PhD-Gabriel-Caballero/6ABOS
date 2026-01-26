f0eb36f835332dfbb28bdf6632689891c83affa9
PhD-Gabriel-Caballero <92304222+PhD-Gabriel-Caballero@users.noreply.github.com>
2026-01-26 19:31:06 +0100

    Update __init__.py

--- src/sixabos/__init__.py
+++ src/sixabos/__init__.py
@@ -24,8 +24,9 @@ from .main import run_6abos
 from .core import SixABOSEngine
 from .config import DEFAULT_CONF
 
-__version__ = "1.1.0"
+__version__ = "1.1.2"
 __author__ = "Gabriel Caballero"
 
 
 print("--- [SYSTEM] 6ABOS Modules successfully hooked ---")
+
